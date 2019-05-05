06067df06ab6b0099518ccae6413eadcb3cc7e48
Rost99933 <rostyslavlysyi@gmail.com>
2019-05-05 12:36:47 +0300

    Initial commit

---
+++ src/main/java/qa_light_selenium_36/App.java
@@ -0,0 +1,13 @@
+package qa_light_selenium_36;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
