e66a73f47e84d770ef3b0107a4d306f0edeb1b76
Rost99933 <rostyslavlysyi@gmail.com>
2019-05-18 13:03:52 +0300

    add new methods and classes

--- src/main/java/pages/ParentPage.java
+++ src/main/java/pages/ParentPage.java
@@ -3,20 +3,23 @@ package pages;
 import libs.ActionsWithWebElements;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 
-public class ParrentPage {
+public class ParentPage {
 
     WebDriver driver;
     Logger log;
     ActionsWithWebElements actionsWithWebElements;
 
-    public ParrentPage(WebDriver driver) {
+
+    public ParentPage(WebDriver driver) {
         this.driver = driver;
         log = Logger.getLogger(getClass());
         actionsWithWebElements = new ActionsWithWebElements(driver);
+        PageFactory.initElements(driver, this);
     }
 
-    public ParrentPage() {
+    public ParentPage() {
     }
 }
